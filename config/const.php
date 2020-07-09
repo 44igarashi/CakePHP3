27dfca461ea64e9fe96edcf4eb2feb03b496e3f5
五十嵐　望 <44igarashi@gmail.com>
2020-07-10 01:38:49 +0900

    constファイルの配置と設定

---
+++ config/const.php
@@ -0,0 +1,15 @@
+<?php
+
+use Cake\Core\Configure;
+
+return [
+
+    // 性別
+    define('MAN',   1), // 男性
+    define('WOMAN', 2), // 女性
+
+    define('GENDERS', [
+        MAN   => '男性',
+        WOMAN => '女性',
+    ]),
+];
