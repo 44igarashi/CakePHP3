614948e02ce43bb1d980be3e8e42354528e3c770
五十嵐　望 <44igarashi@gmail.com>
2020-07-12 00:48:41 +0900

    定数：削除フラグの追加

--- config/const.php
+++ config/const.php
@@ -4,12 +4,7 @@ use Cake\Core\Configure;
 
 return [
 
-    // 性別
-    define('MAN',   1), // 男性
-    define('WOMAN', 2), // 女性
-
-    define('GENDERS', [
-        MAN   => '男性',
-        WOMAN => '女性',
-    ]),
+    // 削除フラグ
+    define('FLG_OFF', 0),
+    define('FLG_ON',  1),
 ];
